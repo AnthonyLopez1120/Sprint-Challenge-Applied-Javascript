fa1572e8ff738e87f2fcf5181a65ec8649677235
Anthony Lopez <anthonybeconscious@gmail.com>
2019-09-06 10:33:27 -0500

    created component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,23 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container')
+function Header() {
+    const headerDiv = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const currentTemp = document.createElement('span');
+
+
+    date.classList.add('date');
+    currentTemp.classList.add('temp');
+
+    date.textContent = 'March 28, 2019'
+
+    headerContainer.appendChild(headerDiv);
+    headerDiv.appendChild(date);
+    headerDiv.appendChild(title);
+    headerDiv.appendChild(currentTemp);
+
+    return Header;
+}
